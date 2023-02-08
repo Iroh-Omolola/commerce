6b2ac0b0e013c61bb436d6ce4a693d2c772d22e7
Iroh-Omolola <irohomolola@gmail.com>
2023-02-08 02:58:03 +0100

    added docker files

--- server/query/index.js
+++ server/query/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
+
 
 const app = express()
 app.use(bodyParser.json())
