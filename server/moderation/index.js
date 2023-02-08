768ee3313e91c902cda84ab7b24a5795c4a7a404
Iroh-Omolola <irohomolola@gmail.com>
2023-02-08 14:02:28 +0100

    added kubernetes file

--- server/moderation/index.js
+++ server/moderation/index.js
@@ -12,7 +12,7 @@ app.post('/events', async(req, res) => {
         const status = data.content.includes('red') ? 'rejected' : 'approved';
   console.log('status', status)
 
-    await axios.post('http://localhost:4005/events',{
+    await axios.post('http://event-srv:4005/events',{
         type: 'CommentModerated',
         data:{
             id: data.id,
--- server/query/index.js
+++ server/query/index.js
@@ -48,7 +48,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async() => {
   console.log('listening on 4002')
 
-  const res = await axios.get('http://localhost:4005/events')
+  const res = await axios.get('http://event-srv:4005/events')
 
   for(let event of res.data){
     handleEvents(event.type, event.data)
